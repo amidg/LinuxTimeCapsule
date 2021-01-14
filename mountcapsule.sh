11cc093f6bce4366431a90175a0db41ace4b83ea
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-01-14 09:05:24 -0800

    minor fix of echo

--- mountcapsule.sh
+++ mountcapsule.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-echo "sudo mount -t cifs //192.168.0.xxx/DISK /media/username/timecapsule -o username=admin,pass=public,sec=ntlm,uid=username,vers=1.0" | /bin/bash 
+echo "sudo mount -t cifs //192.168.0.xxx/DISK-NAME /media/username/timecapsule -o username=admin,pass=public,sec=ntlm,uid=username,vers=1.0" | /bin/bash 
 
 #kde users only:
 kdialog --title "Apple Time Capsule" --passivepopup "Network Storage mounted" 5
